ccc93fb225148c43374625efc8dc341fb56e0ea9
Jeremy Watt <jermwatt@gmail.com>
2025-02-22 13:29:14 -0700

    profiler added

--- network_vanilla/config.py
+++ network_vanilla/config.py
@@ -3,13 +3,13 @@ INPUT_SIZE = 784
 NUM_CLASSES = 10
 LEARNING_RATE = 0.001
 BATCH_SIZE = 64
-NUM_EPOCHS = 3
+NUM_EPOCHS = 1
 
 # Dataset
 DATA_DIR = "dataset/"
 NUM_WORKERS = 0
 
 # Compute related
-ACCELERATOR = "cpu"
-DEVICES = [0]
+ACCELERATOR = "mps"
+DEVICES = 1
 PRECISION = "16-mixed"
--- network_vanilla/train.py
+++ network_vanilla/train.py
@@ -1,10 +1,26 @@
+import torch
 import pytorch_lightning as pl
 from blocks import FullyConnectedBlock
 from model import NN
 from dataset import MnistDataModule
+from callbacks import MyPrintingCallback, EarlyStopping
 import config
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.profilers import PyTorchProfiler
+
+torch.set_float32_matmul_precision("medium") # to make lightning happy
+
 
 if __name__ == "__main__":
+    # set profiler
+    profiler = PyTorchProfiler(
+        on_trace_ready=torch.profiler.tensorboard_trace_handler("profiler_logs/profiler0"),
+        schedule=torch.profiler.schedule(skip_first=10, wait=1, warmup=1, active=20),
+    )
+
+    # setup logger
+    logger = TensorBoardLogger("tb_logs", name="mnist_model_v1")
+
     # Initialize network
     fully_connected_block = FullyConnectedBlock(input_size=config.INPUT_SIZE, num_classes=config.NUM_CLASSES)
     model = NN(fully_connected_block=fully_connected_block, num_classes=config.NUM_CLASSES)
@@ -18,11 +34,13 @@ if __name__ == "__main__":
 
     # initialize trainer
     trainer = pl.Trainer(
+        profiler=profiler,
         accelerator=config.ACCELERATOR,
         # devices=config.DEVICES, # only use for gpu
         min_epochs=1,
-        max_epochs=3,
+        max_epochs=config.NUM_EPOCHS,
         precision=config.PRECISION,
+        callbacks=[MyPrintingCallback(), EarlyStopping(monitor="val_loss")],
     )
     trainer.fit(model, dm)
     trainer.validate(model, dm)
